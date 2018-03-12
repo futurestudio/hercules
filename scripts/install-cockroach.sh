fd5fb1159efd14edb925e3a28550866ec92fbbe6
Marcus Poehls <marcus.poehls@gmail.com>
2018-03-12 11:35:17 +0100

    bind cockroachdb on all IP addresses

--- scripts/install-cockroach.sh
+++ scripts/install-cockroach.sh
@@ -6,7 +6,5 @@
 wget -qO- https://binaries.cockroachdb.com/cockroach-v1.1.5.linux-amd64.tgz | tar  xvz
 sudo cp -i cockroach-v1.1.5.linux-amd64/cockroach /usr/local/bin
 
-# config
-
-# start
-sudo cockroach start --insecure --host=localhost --http-port=8090
+# start with config
+sudo cockroach start --background --insecure --http-port=8090
