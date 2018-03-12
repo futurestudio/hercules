4a56540d369a8c37f982ebc1ffef2f2aa5f9d6a7
Marcus Poehls <marcus.poehls@gmail.com>
2018-03-12 11:36:02 +0100

    install Erlang for RabbitMQ

--- scripts/install-rabbit.sh
+++ scripts/install-rabbit.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/env bash
 
-# RabbitMQ
+# RabbitMQ with Erlang
 
-# install
+# install Erlang
+sudo echo "deb http://binaries.erlang-solutions.com/debian xenial contrib" > /etc/apt/sources.list.d/eslerlang.list
+wget -O - http://binaries.erlang-solutions.com/debian/erlang_solutions.asc | sudo apt-key add -
+sudo apt-get install -y esl-erlang
+
+# install RabbitMQ
 sudo echo "deb https://dl.bintray.com/rabbitmq/debian xenial main" | sudo tee /etc/apt/sources.list.d/bintray.rabbitmq.list
 wget -O- https://www.rabbitmq.com/rabbitmq-release-signing-key.asc | sudo apt-key add -
 sudo apt-get update
 sudo apt-get install -y rabbitmq-server
 
 # config
-
 
 # start
 sudo systemctl restart rabbitmq-server
