fb5c31ea2d8bd4e5dc4195047374b54dce3c8229
Marcus Poehls <marcus.poehls@gmail.com>
2018-03-12 23:04:18 +0100

    reformat sleep command (eslint)

--- commands/sleep.js
+++ commands/sleep.js
@@ -7,18 +7,17 @@ const { Command } = require('@adonisjs/ace')
 
 const UserHomeDir = Os.homedir()
 const HometownDir = Path.resolve(UserHomeDir, 'Hometown')
-const Vagrantfile = Path.resolve(__dirname, '..', 'Vagrantfile')
 
 class Sleep extends Command {
-  static get signature() {
+  static get signature () {
     return 'sleep'
   }
 
-  static get description() {
+  static get description () {
     return 'Suspend your hometown box'
   }
 
-  async handle() {
+  async handle () {
     const result = Execa('vagrant', ['suspend'], { cwd: HometownDir })
     result.stdout.pipe(process.stdout)
     result.stderr.pipe(process.stderr)
