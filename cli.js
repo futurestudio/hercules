882a4335c4ebaba885e7b987b438e2a74d836e6f
Marcus Poehls <marcus.poehls@gmail.com>
2018-03-13 07:17:47 +0100

    add update notifier

--- cli.js
+++ cli.js
@@ -1,7 +1,12 @@
 #!/usr/bin/env node
 'use strict'
 
+const Path = require('path')
 const Ace = require('@adonisjs/ace')
+const UpdateNotifier = require('update-notifier')
+const Pkg = require(Path.resolve(__dirname, 'package.json'))
+
+UpdateNotifier({ Pkg }).notify()
 
 Ace.addCommand(require('./commands/up'))
 Ace.addCommand(require('./commands/lift'))
