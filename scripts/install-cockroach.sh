af97ca132c87aba61ff618197ca537b282041e53
Marcus Poehls <marcus.poehls@gmail.com>
2018-06-25 11:01:27 +0200

    install Cockroachdb 2.0

--- scripts/install-cockroach.sh
+++ scripts/install-cockroach.sh
@@ -3,8 +3,11 @@
 # CockroachDB
 
 # install
-wget -qO- https://binaries.cockroachdb.com/cockroach-v1.1.5.linux-amd64.tgz | tar  xvz
-sudo cp -i cockroach-v1.1.5.linux-amd64/cockroach /usr/local/bin
+sudo apt-get update
+sudo apt-get install -y glibc libncurses
+
+wget -qO- https://binaries.cockroachdb.com/cockroach-v2.0.3.linux-amd64.tgz | tar  xvz
+sudo cp -i cockroach-v2.0.3.linux-amd64/cockroach /usr/local/bin
 
 # start with config
 sudo cockroach start --background --insecure --http-port=8090
