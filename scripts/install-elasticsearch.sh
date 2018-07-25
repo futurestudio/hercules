88c10fffac738ae4207d1215dc7bb87635ae41ce
Marcus Poehls <marcus.poehls@gmail.com>
2018-07-25 09:14:54 +0200

    update comments

--- scripts/install-elasticsearch.sh
+++ scripts/install-elasticsearch.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-# Elastic
+# ElasticSearch
 
-# install
-## first, Java
+## Install
+### first, Java
 sudo apt-get update
 sudo apt-get install -y default-jdk
 
@@ -11,25 +11,25 @@ export JAVA_HOME=/usr/lib/jvm/java-8-oracle
 export PATHADD=$JAVA_HOME/bin
 export PATH=$PATH:$PATHADD
 
-## second, Elastic
+### second, Elastic
 wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
 sudo apt-get install apt-transport-https
 echo "deb https://artifacts.elastic.co/packages/6.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-6.x.list
 sudo apt-get update && sudo apt-get install -y elasticsearch
 
 
-## configure
-# Start Elasticsearch on boot
+## Configure
+### Start Elasticsearch on boot
 
 sudo update-rc.d elasticsearch defaults 95 10
 
-# Use 'hometown' as the cluster
+## Use 'hometown' as the cluster
 sudo sed -i "s/#cluster.name: my-application/cluster.name: hometown/" /etc/elasticsearch/elasticsearch.yml
-# Bind IP to access from host
+## Bind IP to access from host
 sudo sed -i "s/#network.host: 192.168.0.1/network.host: 0.0.0.0/" /etc/elasticsearch/elasticsearch.yml
 
 
-# start with config
+## Start
 sudo systemctl daemon-reload
 sudo systemctl enable elasticsearch.service
 sudo systemctl restart elasticsearch.service
--- scripts/install-maria.sh
+++ scripts/install-maria.sh
@@ -1,25 +1,27 @@
 #!/usr/bin/env bash
 
-# Install MariaDB, non-interactive
+# MariaDB,
 
+## Install non-interactive
 export DEBIAN_FRONTEND=noninteractive
 
-# Add Maria PPA
+
+### Add Maria PPA
 sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8
 sudo add-apt-repository 'deb [arch=amd64,i386,ppc64el] http://ftp.osuosl.org/pub/mariadb/repo/10.2/ubuntu xenial main'
 sudo apt-get update
 
-# Set the automated root password
+#### Set the automated root password
 export DEBIAN_FRONTEND=noninteractive
 
 debconf-set-selections <<< "mariadb-server-10.2 mysql-server/data-dir select ''"
 debconf-set-selections <<< "mariadb-server-10.2 mysql-server/root_password password secret"
 debconf-set-selections <<< "mariadb-server-10.2 mysql-server/root_password_again password secret"
 
-# install
 sudo apt-get install -y mariadb-server
 
-# Configure Maria Remote Access
+
+## Configure Maria Remote Access
 sed -i '/^bind-address/s/bind-address.*=.*/bind-address = 0.0.0.0/' /etc/mysql/my.cnf
 
 mysql --user="root" --password="secret" -e "GRANT ALL ON *.* TO root@'0.0.0.0' IDENTIFIED BY 'secret' WITH GRANT OPTION;"
@@ -29,4 +31,7 @@ mysql --user="root" --password="secret" -e "CREATE USER 'hometown'@'0.0.0.0' IDE
 mysql --user="root" --password="secret" -e "GRANT ALL ON *.* TO 'hometown'@'0.0.0.0' IDENTIFIED BY 'secret' WITH GRANT OPTION;"
 mysql --user="root" --password="secret" -e "GRANT ALL ON *.* TO 'hometown'@'%' IDENTIFIED BY 'secret' WITH GRANT OPTION;"
 mysql --user="root" --password="secret" -e "FLUSH PRIVILEGES;"
+
+
+## Start
 sudo systemctl restart mysql
