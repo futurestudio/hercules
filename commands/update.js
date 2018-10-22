638ee6f48fbe1ca9d7ad2a393c0bbe8412ea5f9c
Marcus Poehls <marcus.poehls@gmail.com>
2018-10-22 17:30:13 +0200

    restart box after update

--- commands/update.js
+++ commands/update.js
@@ -15,25 +15,32 @@ class Update extends BaseCommand {
 
   async handle () {
     await this.run(async () => {
-      await this.suspendBox()
-      await this.updateBox()
+      await this.suspend()
+      await this.update()
+      await this.start()
     })
   }
 
-  async suspendBox () {
+  async suspend () {
     const spinner = Ora('Suspending the box').start()
     await Execa('hercules', ['sleep'])
-    spinner.succeed('Box suspended.')
+    spinner.succeed('Box suspended.\n')
   }
 
-  async updateBox () {
-    this.info('Starting the update')
+  async start () {
+    const spinner = Ora('Starting the box').start()
+    await Execa('hercules', ['up'])
+    spinner.succeed('Box started and ready to use.')
+  }
+
+  async update () {
+    this.info('Starting the update\n')
     await this.copyVagrantfile()
     await this.copyScripts()
 
-    const update = Execa('vagrant', ['up', '--provision'], { cwd: this.herculesDir() })
-    update.stdout.pipe(process.stdout)
-    update.stderr.pipe(process.stderr)
+    // const update = Execa('vagrant', ['up', '--provision'], { cwd: this.herculesDir() })
+    // update.stdout.pipe(process.stdout)
+    // update.stderr.pipe(process.stderr)
   }
 }
 
