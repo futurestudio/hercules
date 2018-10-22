1708e79e6c0ae945595aad42471797ec4925ddb7
Marcus Poehls <marcus.poehls@gmail.com>
2018-10-22 13:58:22 +0200

    update command import

--- cli.js
+++ cli.js
@@ -3,18 +3,13 @@
 
 const Path = require('path')
 const Ace = require('@adonisjs/ace')
+const Commands = require('./commands')
 const UpdateNotifier = require('update-notifier')
 const Pkg = require(Path.resolve(__dirname, 'package.json'))
 
 UpdateNotifier({ pkg: Pkg }).notify()
 
-Ace.addCommand(require('./commands/up'))
-Ace.addCommand(require('./commands/lift'))
-Ace.addCommand(require('./commands/suspend'))
-Ace.addCommand(require('./commands/sleep'))
-Ace.addCommand(require('./commands/destroy'))
-Ace.addCommand(require('./commands/status'))
-Ace.addCommand(require('./commands/restart'))
+Object.keys(Commands).forEach(Command => Ace.addCommand(Commands[Command]))
 
 // Boot ace to execute commands
 Ace.wireUpWithCommander()
---
+++ commands/index.js
@@ -0,0 +1,10 @@
+'use strict'
+
+module.exports = {
+  up: require('./up'),
+  init: require('./init'),
+  sleep: require('./sleep'),
+  status: require('./status'),
+  restart: require('./restart'),
+  destroy: require('./destroy')
+}
