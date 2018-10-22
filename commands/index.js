6d47fb80f95d494fc742b98ef2ef69fd60f0b2d4
Marcus Poehls <marcus.poehls@gmail.com>
2018-10-22 17:22:17 +0200

    add update command

--- commands/index.js
+++ commands/index.js
@@ -4,6 +4,7 @@ module.exports = {
   up: require('./up'),
   init: require('./init'),
   sleep: require('./sleep'),
+  update: require('./update'),
   status: require('./status'),
   restart: require('./restart'),
   destroy: require('./destroy')
