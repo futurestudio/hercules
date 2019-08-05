e519fd9da17f7fea30c3088b50d1f1d8eb13fc68
Marcus Poehls <marcus.poehls@gmail.com>
2019-08-05 13:14:51 +0200

    fix RethinkDB install

--- scripts/install-rethinkdb.sh
+++ scripts/install-rethinkdb.sh
@@ -12,14 +12,12 @@ touch /home/vagrant/.hercules-installed/rethinkdb
 
 
 # install
-source /etc/lsb-release && echo "deb http://download.rethinkdb.com/apt $DISTRIB_CODENAME main" | sudo tee /etc/apt/sources.list.d/rethinkdb.list
-wget -qO- https://download.rethinkdb.com/apt/pubkey.gpg | sudo apt-key add -
-sudo apt-get update --allow-unauthenticated
-
-sudo apt-get install -y --allow-unauthenticated rethinkdb
+wget https://github.com/srh/rethinkdb/releases/download/v2.3.6.srh.1/rethinkdb_2.3.6.srh.1.0bionic_amd64.deb
+sudo dpkg -i rethinkdb_2.3.6.srh.1.0bionic_amd64.deb
 
 # config
-sudo sed -e 's/# bind=127.0.0.1/bind=all/g' /etc/rethinkdb/default.conf.sample > /etc/rethinkdb/instances.d/default.conf
+sudo cp /etc/rethinkdb/default.conf.sample /etc/rethinkdb/instances.d/default.conf
+sudo sed -i 's/# bind=127.0.0.1/bind=all/g' /etc/rethinkdb/instances.d/default.conf
 sudo rethinkdb create -d /var/lib/rethinkdb/instances.d/default 2>&1
 
 # start
