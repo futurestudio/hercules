1e63dc2d05cd83ab86b7bf8cbf3b51fea635e37e
Marcus Pöhls <marcus.poehls@gmail.com>
2019-08-05 15:16:52 +0200

    create folder on provisioning

--- scripts/update-system.sh
+++ scripts/update-system.sh
@@ -15,3 +15,6 @@ locale-gen en_US.UTF-8
 # Install common packages
 apt-get install -y software-properties-common curl wget htop m4 g++
 apt-get install -y build-essential dos2unix gcc libmcrypt4 libpcre3-dev ntp unzip make python2.7-dev python-pip unattended-upgrades whois libnotify-bin pv cifs-utils mcrypt bash-completion zsh protobuf-compiler libprotobuf-dev libcurl4-openssl-dev libboost-all-dev libncurses5-dev libjemalloc-dev
+
+# Create directory storing all installed services
+mkdir /home/vagrant/.hercules-installed
