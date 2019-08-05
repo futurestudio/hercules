0e4496c80d67952df4be2ffde21b9fc5597c44de
Marcus Poehls <marcus.poehls@gmail.com>
2019-08-05 12:10:22 +0200

    install htop

--- scripts/update-system.sh
+++ scripts/update-system.sh
@@ -13,5 +13,5 @@ echo "LC_ALL=en_US.UTF-8" >> /etc/default/locale
 locale-gen en_US.UTF-8
 
 # Install common packages
-apt-get install -y software-properties-common curl wget
+apt-get install -y software-properties-common curl wget htop
 apt-get install -y build-essential dos2unix gcc libmcrypt4 libpcre3-dev ntp unzip make python2.7-dev python-pip unattended-upgrades whois libnotify-bin pv cifs-utils mcrypt bash-completion zsh
