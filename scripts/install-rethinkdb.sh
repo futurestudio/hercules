ecf78c246c8d685474e244d2abd568b562123c81
Marcus Poehls <marcus.poehls@gmail.com>
2019-08-05 12:10:38 +0200

    --allow-unauthenticated

--- scripts/install-rethinkdb.sh
+++ scripts/install-rethinkdb.sh
@@ -14,9 +14,9 @@ touch /home/vagrant/.hercules-installed/rethinkdb
 # install
 source /etc/lsb-release && echo "deb http://download.rethinkdb.com/apt $DISTRIB_CODENAME main" | sudo tee /etc/apt/sources.list.d/rethinkdb.list
 wget -qO- https://download.rethinkdb.com/apt/pubkey.gpg | sudo apt-key add -
-sudo apt-get update
+sudo apt-get update --allow-unauthenticated
 
-sudo apt-get install -y rethinkdb
+sudo apt-get install -y --allow-unauthenticated rethinkdb
 
 # config
 sudo sed -e 's/# bind=127.0.0.1/bind=all/g' /etc/rethinkdb/default.conf.sample > /etc/rethinkdb/instances.d/default.conf
