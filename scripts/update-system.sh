1980912b8f391d703cc461f0c5c0aa3c86e9b2af
Marcus Poehls <marcus.poehls@gmail.com>
2019-08-05 13:14:28 +0200

    install tools

--- scripts/update-system.sh
+++ scripts/update-system.sh
@@ -13,5 +13,5 @@ echo "LC_ALL=en_US.UTF-8" >> /etc/default/locale
 locale-gen en_US.UTF-8
 
 # Install common packages
-apt-get install -y software-properties-common curl wget htop
-apt-get install -y build-essential dos2unix gcc libmcrypt4 libpcre3-dev ntp unzip make python2.7-dev python-pip unattended-upgrades whois libnotify-bin pv cifs-utils mcrypt bash-completion zsh
+apt-get install -y software-properties-common curl wget htop m4 g++
+apt-get install -y build-essential dos2unix gcc libmcrypt4 libpcre3-dev ntp unzip make python2.7-dev python-pip unattended-upgrades whois libnotify-bin pv cifs-utils mcrypt bash-completion zsh protobuf-compiler libprotobuf-dev libcurl4-openssl-dev libboost-all-dev libncurses5-dev libjemalloc-dev
